c4f21822ab046f6261f6f49ba9c51466a18b6e68
915dbfl <dbfl00915@naver.com>
2024-03-30 15:31:23 +0900

    [FEAT] suggestionDashboard viewModel(inputs, outputs), activity(BaseComposeActivity) 수정 (#221)

--- feature/mission_suggestion/src/main/java/com/lgtm/android/mission_suggestion/ui/dashboard/SuggestionDashboardViewModel.kt
+++ feature/mission_suggestion/src/main/java/com/lgtm/android/mission_suggestion/ui/dashboard/SuggestionDashboardViewModel.kt
@@ -2,13 +2,15 @@ package com.lgtm.android.mission_suggestion.ui.dashboard
 
 import android.content.ContentValues.TAG
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import com.lgtm.android.common_ui.base.BaseViewModel
 import com.lgtm.android.common_ui.model.mapper.toUiModel
+import com.lgtm.android.common_ui.util.UiState
+import com.lgtm.android.mission_suggestion.ui.dashboard.presentation.contract.SuggestionDashboardInputs
+import com.lgtm.android.mission_suggestion.ui.dashboard.presentation.contract.SuggestionDashboardOutputs
+import com.lgtm.android.mission_suggestion.ui.dashboard.presentation.contract.SuggestionDashboardUiEffect
 import com.lgtm.domain.constants.Role
 import com.lgtm.domain.mission_suggestion.SuggestionContent
 import com.lgtm.domain.mission_suggestion.SuggestionVO
@@ -16,6 +18,10 @@ import com.lgtm.domain.mission_suggestion.SuggestionViewType
 import com.lgtm.domain.repository.AuthRepository
 import com.lgtm.domain.usecase.SuggestionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,18 +29,23 @@ import javax.inject.Inject
 class SuggestionDashboardViewModel @Inject constructor(
     private val suggestionUseCase: SuggestionUseCase,
     authRepository: AuthRepository
-): BaseViewModel() {
+): BaseViewModel(), SuggestionDashboardInputs, SuggestionDashboardOutputs {
 
-    val suggestionBtnVisibility: Boolean = authRepository.getMemberType() == Role.REVIEWEE
+    val createBtnVisibility: Boolean = authRepository.getMemberType() == Role.REVIEWEE
 
-    private val _suggestionList = MutableLiveData<List<SuggestionContent>>()
-    val suggestionList: LiveData<List<SuggestionContent>> = _suggestionList
+    private val _suggestionDashboardState = MutableStateFlow<UiState<List<SuggestionContent>>>(UiState.Init)
+    override val suggestionDashboardState: StateFlow<UiState<List<SuggestionContent>>>
+        get() = _suggestionDashboardState
+
+    private val _suggestionDashboardUiEffect: MutableSharedFlow<SuggestionDashboardUiEffect> = MutableSharedFlow(replay = 0)
+    override val suggestionDashboardUiEffect: SharedFlow<SuggestionDashboardUiEffect>
+        get() = _suggestionDashboardUiEffect
 
     fun fetchSuggestionList() {
         viewModelScope.launch(lgtmErrorHandler) {
             suggestionUseCase.getSuggestionList()
                 .onSuccess {
-                    _suggestionList.postValue(convertToUiModel(it))
+                    _suggestionDashboardState.value = UiState.Success(data = convertToUiModel(it))
                     Log.d(TAG, "getSuggestion: $it")
                 }.onFailure {
                     Firebase.crashlytics.recordException(it)
@@ -49,6 +60,32 @@ class SuggestionDashboardViewModel @Inject constructor(
                SuggestionViewType.CONTENT -> (it as SuggestionVO).toUiModel()
                else -> it
             }
+        }
+    }
+
+    override fun likeSuggestion() {
+        TODO("Not yet implemented")
+    }
+
+    override fun cancelLikeSuggestion() {
+        TODO("Not yet implemented")
+    }
+
+    override fun moveToCreateSuggestion() {
+        viewModelScope.launch(lgtmErrorHandler) {
+            _suggestionDashboardUiEffect.emit(SuggestionDashboardUiEffect.CreateSuggestion)
+        }
+    }
+
+    override fun moveToSuggestionDetail(suggestionId: Int) {
+        viewModelScope.launch(lgtmErrorHandler) {
+            _suggestionDashboardUiEffect.emit(SuggestionDashboardUiEffect.SuggestionDetail(suggestionId))
+        }
+    }
+
+    override fun goBack() {
+        viewModelScope.launch(lgtmErrorHandler) {
+            _suggestionDashboardUiEffect.emit(SuggestionDashboardUiEffect.GoBack)
         }
     }
 
